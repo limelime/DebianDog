3e656a2796b19bc002a660074e761ca93f103cab
xuanngo <xuanngo@users.noreply.github.com>
2015-02-23 10:53:13 -0500

    Add post-boot/ in /root/

--- debian-dog-remaster/mk-custom-squashfs.sh
+++ debian-dog-remaster/mk-custom-squashfs.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
-# Description: 
+# Description: Repackage squashfs with post-boot/ directory in /root
 # Author: Xuan Ngo
 # Usage: mk-custom-squashfs.sh <DebianDog Original ISO> <Output Squash File>
-# Reference: 
+# Requirements:
+ #  - mksquashfs
 
 DEBIAN_DOG_ISO=$1
-OUTPUT_SQUASH_FILE=$1
+OUTPUT_SQUASH_FILE=$2
 
+### Error handling
+if [ $# -eq 0 ]; then
+  echo "ERROR: <DebianDog Original ISO> <Output Squash File> arguments are missing!"
+  echo "Example:"
+  echo "\t ./mk-custom-squashfs.sh /path-to/DebianDog-Wheezy-openbox_xfce.iso /path-where-to-create-squash-file"
+  exit 1;
+fi
+
+### Variables
 WORKING_DIR=/tmp
 MOUNT_DEBIAN_DOG_ISO=${WORKING_DIR}/mnt-debian-dog-iso
 MOUNT_SQUASH=${WORKING_DIR}/mnt-squash
@@ -25,7 +35,15 @@ rm -rf ${NEW_SQUASH_DIR}
 mkdir -p ${NEW_SQUASH_DIR}
 cp -R ${MOUNT_SQUASH}/* ${NEW_SQUASH_DIR}
 
+### Copy post-boot directory to /root.
+cp -R post-boot ${NEW_SQUASH_DIR}/root/
+
 
 ### Clean up
+lsof | grep iso
 umount ${MOUNT_DEBIAN_DOG_ISO}
 umount ${MOUNT_SQUASH}
+umount ${MOUNT_DEBIAN_DOG_ISO}
+umount ${MOUNT_SQUASH}
+#rm -rf ${MOUNT_DEBIAN_DOG_ISO}
+#rm -rf ${MOUNT_SQUASH}
