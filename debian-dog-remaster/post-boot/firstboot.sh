270adcd0515d529d59a1aab5c84ce8bf667d1014
limelime <limelime@users.noreply.github.com>
2015-02-23 11:38:50 -0500

    Install scite.

--- debian-dog-remaster/post-boot/firstboot.sh
+++ debian-dog-remaster/post-boot/firstboot.sh
@@ -11,13 +11,10 @@ cp ${POST_BOOT_DIR}/bashrc_root /root/.bashrc
 DEB_CACHE_DIR=/var/cache/apt/archives/
 cp -Rf ${POST_BOOT_DIR}/apt-i386/archives/* ${DEB_CACHE_DIR} 
 
-### Modify sources.list
-# Disable the cdrom path from the apt sources.list automatically.
-sed -i 's/^deb cdrom/#deb/' /etc/apt/sources.list
-cat ${POST_BOOT_DIR}/add-sources.list >> /etc/apt/sources.list
-aptitude -y update
+### Install packages/applications
+apt-get update
+  
+sh install-scite.sh
+sh install-chinese-fonts.sh
 
-### Install other packages
-# These packages are need to install VirtualBox Guest Additions.
-	aptitude -y install bzip2
-	aptitude -y install dkms build-essential linux-headers-$(uname -r)
+
---
+++ debian-dog-remaster/post-boot/install-scite.sh
@@ -0,0 +1,23 @@
+INSTALL_DATA_DIR=/root/post-boot/install-data
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+# Remove current default text editor
+apt-get remove leafpad
+
+# Install Scite
+apt-get -y intsall scite
+
+# Modify Scite configurations
+SCITE_CONFIG_FILE=/etc/scite/SciTEGlobal.properties
+if [ ! -f ${SCITE_CONFIG_FILE}.bck ]; then
+	cp -n ${SCITE_CONFIG_FILE} ${SCITE_CONFIG_FILE}.bck
+	cat ${INSTALL_DATA_DIR}/scite-config.txt >> ${SCITE_CONFIG_FILE}
+	
+	# Debug: sed "s/open.filter=\\\/open.filter=\\\xuan\\n\$(all.files)\\\xuan\\nxuan/" /etc/scite/SciTEGlobal.properties | grep xuan
+  sed -i "s/open.filter=\\\/open.filter=\\\\\n\$(all.files)\\\\\n/" /etc/scite/SciTEGlobal.properties
+
+else
+  echo "Error: ${SCRIPT_NAME} failed!"
+  echo "${SCITE_CONFIG_FILE}.bck already exists."
+fi
+
