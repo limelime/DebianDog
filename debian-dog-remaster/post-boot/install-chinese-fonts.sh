884d0d85e0b747835bd93098cb722e241f390038
limelime <limelime@users.noreply.github.com>
2015-02-23 17:33:43 -0500

    update.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -4,17 +4,22 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 # Install locale
 # -http://stackoverflow.com/a/20693661
 apt-get -y install locales
-export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
-debconf-set-selections ${INSTALL_DATA_DIR}/locale-config.seed
-dpkg-reconfigure locales
+LOCALE_GEN=/etc/locale.gen
+cp -n ${LOCALE_GEN} ${LOCALE_GEN}.bck
+echo "en_US.UTF-8 UTF-8" >> ${LOCALE_GEN}
+/usr/sbin/locale-gen
 
 # Install Chinese fonts so that it display correctly in Iceweseal/Firefox and in the console.
 apt-get -y install fonts-arphic-ukai fonts-arphic-uming
 
 # Install localepurge to keep locale small
 apt-get -y install localepurge
+echo "localepurge localepurge/nopurge multiselect en, en_US.UTF-8, zh, zh_TW, zh_TW.UTF-8"|debconf-set-selections
+dpkg-reconfigure localepurge
 
 # References:
 # -https://people.debian.org/~schultmc/locales.html
 # -http://perlgeek.de/en/article/set-up-a-clean-utf8-environment
 # /etc/default/locale
+# -http://superuser.com/questions/715722/how-to-do-dpkg-reconfigure-dash-as-bash-automatically
+# -http://blog.nutsfactory.net/2008/03/06/noninteractive-dpkg-installation-on-debian-system/
