236309bf92b3bb7b9084383841f237d8b351a47b
limelime <limelime@users.noreply.github.com>
2015-02-24 12:48:42 -0500

    Using "dpkg-reconfigure locales" doesn't work.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -1,6 +1,9 @@
 INSTALL_DATA_DIR=/root/post-boot/install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
+# Setup debconf parameters
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true DEBCONF_DEBUG=developer
+
 # Install locale
 # -http://stackoverflow.com/a/20693661
 apt-get -y install locales
@@ -9,13 +12,19 @@ cp -n ${LOCALE_GEN} ${LOCALE_GEN}.bck
 echo "en_US.UTF-8 UTF-8" >> ${LOCALE_GEN}
 /usr/sbin/locale-gen
 
+# Install localepurge to keep locale small
+apt-get -y install localepurge
+debconf-set-selections ${INSTALL_DATA_DIR}/localepurge-config.seed
+dpkg-reconfigure localepurge
+
 # Install Chinese fonts so that it display correctly in Iceweseal/Firefox and in the console.
 apt-get -y install fonts-arphic-ukai fonts-arphic-uming
 
-# Install localepurge to keep locale small
-apt-get -y install localepurge
-echo "localepurge localepurge/nopurge multiselect en, en_US.UTF-8" | debconf-set-selections
-DEBCONF_DEBUG=developer DEBIAN_FRONTEND=noninteractive dpkg-reconfigure localepurge
+# Debug
+# cat /etc/default/locale
+# cat /etc/locale.nopurge
+# cat /etc/locale.gen
+
 
 # References:
 # -https://people.debian.org/~schultmc/locales.html
