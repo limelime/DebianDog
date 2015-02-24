805d6e13cc8ef4f0ae82f18da98a91376f461c13
limelime <limelime@users.noreply.github.com>
2015-02-24 17:02:20 -0500

    Manually install locale en_US.UTF-8.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -11,6 +11,10 @@ LOCALE_GEN=/etc/locale.gen
 cp -n ${LOCALE_GEN} ${LOCALE_GEN}.bck
 echo "en_US.UTF-8 UTF-8" >> ${LOCALE_GEN}
 /usr/sbin/locale-gen
+### Gave up on these:
+# update-locale LANG=en_US.UTF-8
+# debconf-set-selections ${INSTALL_DATA_DIR}/locale-config.seed
+# dpkg-reconfigure locales
 
 # Install localepurge to keep locale small
 apt-get -y install localepurge
@@ -32,3 +36,6 @@ apt-get -y install fonts-arphic-ukai fonts-arphic-uming
 # /etc/default/locale
 # -http://superuser.com/questions/715722/how-to-do-dpkg-reconfigure-dash-as-bash-automatically
 # -http://blog.nutsfactory.net/2008/03/06/noninteractive-dpkg-installation-on-debian-system/
+
+# update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 LC_TIME=en_US.UTF-8 LC_CTYPE=en_US.UTF-8
+#   Explained: https://wiki.gentoo.org/wiki/Localization/HOWTO#Environment_variables_for_locales
