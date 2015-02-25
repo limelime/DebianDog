bf98f118c8760e511391a7db11ca7b971547ced4
limelime <limelime@users.noreply.github.com>
2015-02-25 14:23:18 -0500

    Reverse back to fonts-arphic-ukai and fonts-arphic-uming because terminal will not display Chinese charaters with hanazono font.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -2,8 +2,9 @@ INSTALL_DATA_DIR=/root/post-boot/install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 ### Install Chinese fonts so that it display correctly in Iceweseal/Firefox and in the console.
-apt-get -y install fonts-hanazono 
+apt-get -y install fonts-arphic-ukai fonts-arphic-uming 
 # fonts-arphic-ukai fonts-arphic-uming
+# fonts-hanazono
 
 ### Test
 # Open http://ctext.org/zh
