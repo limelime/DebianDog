a20049dab2c0fefa0af62875d085046e9ffb3ca0
limelime <limelime@users.noreply.github.com>
2015-02-25 10:49:55 -0500

    use fonts-hanazono instead.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -2,11 +2,14 @@ INSTALL_DATA_DIR=/root/post-boot/install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 ### Install Chinese fonts so that it display correctly in Iceweseal/Firefox and in the console.
-apt-get -y install fonts-arphic-ukai fonts-arphic-uming
+apt-get -y install fonts-hanazono 
+# fonts-arphic-ukai fonts-arphic-uming
 
 ### Test
 # Open http://ctext.org/zh
+# Open http://ctext.org/font-test-page
 
 
-
+### Varia
+# http://fonts.jp/hanazono/
 # Other languages: http://meta.wikimedia.org/wiki/List_of_Wikipedias
