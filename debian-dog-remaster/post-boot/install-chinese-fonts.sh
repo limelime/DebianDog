5a32803398f5857adea53804e3d751927ae7e0e3
limelime <limelime@users.noreply.github.com>
2015-02-25 10:22:44 -0500

    Use better website.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -5,4 +5,8 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 apt-get -y install fonts-arphic-ukai fonts-arphic-uming
 
 ### Test
-# Open http://meta.wikimedia.org/wiki/List_of_Wikipedias
+# Open http://ctext.org/zh
+
+
+
+# Other languages: http://meta.wikimedia.org/wiki/List_of_Wikipedias
