c0ab9886c07fdb1cffca31818496abd33f7f2bab
limelime <limelime@users.noreply.github.com>
2015-02-25 15:12:14 -0500

    Add install virtualbox guest additions.

---
+++ debian-dog-remaster/post-boot/install-virtualbox.sh
@@ -0,0 +1,21 @@
+INSTALL_DATA_DIR=/root/post-boot/install-data
+SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+### Install VirtualBox Guest Additions.
+
+# Install needed packages to install guest additions.
+apt-get -y install bzip2
+apt-get -y install dkms build-essential linux-headers-$(uname -r)
+
+# Mount VBoxGuestAdditions.iso
+VBOX_GUEST_ISO_DIR=./tmp-vbox-guest-addtions
+mount -o loop VBoxGuestAdditions.iso ${VBOX_GUEST_ISO_DIR}
+
+# Execute VBoxLinuxAdditions.run
+cd ${VBOX_GUEST_ISO_DIR}
+./VBoxLinuxAdditions.run
+cd ..
+
+# Clean up.
+umount ${VBOX_GUEST_ISO_DIR}
+rm -rf ${VBOX_GUEST_ISO_DIR}
--- debian-dog-remaster/post-boot/install-virtualbox.xsh
+++
@@ -1,6 +0,0 @@
-INSTALL_DATA_DIR=/root/post-boot/install-data
-SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-
-# Install VirtualBox Guest Additions.
-apt-get -y install bzip2
-apt-get -y install dkms build-essential linux-headers-$(uname -r)
