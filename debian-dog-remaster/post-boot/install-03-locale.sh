bec9232182c99228e02df0b7c91db9b8a6f7f5f7
limelime <limelime@users.noreply.github.com>
2015-02-25 12:38:55 -0500

    Improve locale.

--- debian-dog-remaster/post-boot/install-03-locale.sh
+++ debian-dog-remaster/post-boot/install-03-locale.sh
@@ -7,12 +7,15 @@ export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true DEBCONF_D
 #### Install locale
 # -http://stackoverflow.com/a/20693661
 apt-get -y install locales
+
 LOCALE_GEN=/etc/locale.gen
 cp -n ${LOCALE_GEN} ${LOCALE_GEN}.bck
 echo "en_US.UTF-8 UTF-8" >> ${LOCALE_GEN}
 /usr/sbin/locale-gen
+
+# So that `cat /etc/default/locale` gives LANG=en_US.UTF-8. 
+update-locale LANG=en_US.UTF-8
 ### Gave up on these:
-# update-locale LANG=en_US.UTF-8
 # debconf-set-selections ${INSTALL_DATA_DIR}/locale-config.seed
 # dpkg-reconfigure locales
 
@@ -20,9 +23,6 @@ echo "en_US.UTF-8 UTF-8" >> ${LOCALE_GEN}
 apt-get -y install localepurge
 debconf-set-selections ${INSTALL_DATA_DIR}/localepurge-config.seed
 dpkg-reconfigure localepurge
-
-### Install Chinese fonts so that it display correctly in Iceweseal/Firefox and in the console.
-apt-get -y install fonts-arphic-ukai fonts-arphic-uming
 
 ### Test
 
