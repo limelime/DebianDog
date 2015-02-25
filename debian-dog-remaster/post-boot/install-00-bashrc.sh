eff1f595c56edf694ac81cf603faf5c6894bea0a
limelime <limelime@users.noreply.github.com>
2015-02-25 10:01:35 -0500

    Add test instructions.

--- debian-dog-remaster/post-boot/install-00-bashrc.sh
+++ debian-dog-remaster/post-boot/install-00-bashrc.sh
@@ -4,3 +4,7 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 ### Add .bashrc.
 cp ${INSTALL_DATA_DIR}/bashrc_root /root/.bashrc
 source /root/.bashrc
+
+
+### Test
+# Open another terminal, you should get color with ls command.
