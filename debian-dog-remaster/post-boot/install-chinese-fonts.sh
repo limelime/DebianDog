2e8882d31735828a4a67b73880f977fed335087c
limelime <limelime@users.noreply.github.com>
2015-02-25 17:30:14 -0500

    Re-instate fonts-hanazono. For it to work in console, reboot.

--- debian-dog-remaster/post-boot/install-chinese-fonts.sh
+++ debian-dog-remaster/post-boot/install-chinese-fonts.sh
@@ -2,7 +2,7 @@ INSTALL_DATA_DIR=/root/post-boot/install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 ### Install Chinese fonts so that it display correctly in Iceweseal/Firefox and in the console.
-apt-get -y install fonts-arphic-ukai fonts-arphic-uming 
+apt-get -y install fonts-hanazono 
 # fonts-arphic-ukai fonts-arphic-uming
 # fonts-hanazono
 
