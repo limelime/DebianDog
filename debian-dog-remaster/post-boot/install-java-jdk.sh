46599fc77a5147800dace8a131760ed7e30bf756
limelime <limelime@users.noreply.github.com>
2015-02-26 13:44:13 -0500

    Force yes on installing jdk.

--- debian-dog-remaster/post-boot/install-java-jdk.sh
+++ debian-dog-remaster/post-boot/install-java-jdk.sh
@@ -2,4 +2,4 @@ INSTALL_DATA_DIR=/root/post-boot/install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 ### Install Java JDK
-apt-get install default-jdk
+apt-get -y install default-jdk
