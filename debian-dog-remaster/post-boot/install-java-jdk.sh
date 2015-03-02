f20e722532f15ebb07680ec8565ed61cb32c67b1
limelime <limelime@users.noreply.github.com>
2015-03-02 12:51:07 -0500

    Instead of installing default-jdk(java6), install jdk7.

--- debian-dog-remaster/post-boot/install-java-jdk.sh
+++ debian-dog-remaster/post-boot/install-java-jdk.sh
@@ -2,4 +2,5 @@ INSTALL_DATA_DIR=./install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 ### Install Java JDK
-apt-get -y install default-jdk
+#apt-get -y install default-jdk
+apt-get -y install openjdk-7-jdk
