4446f0ad3a3c02b7b124a262d1090a3484811d15
limelime <limelime@users.noreply.github.com>
2015-03-04 15:55:21 -0500

    Remove moving kernel files to backup/ because it will cause kernel panic.

--- debian-dog-remaster/add-new-kernel-3.2.0-4-686-Pae.sh
+++ debian-dog-remaster/add-new-kernel-3.2.0-4-686-Pae.sh
@@ -14,13 +14,6 @@ LIVE_CFG=$2
 KERNELS_DIR=kernels
 BOOT_ENTRIES_DIR=./boot-entries
 
-### Backup default kernel files
-BCK_KERNEL_DIR=${LIVE_DIR}/backup
-mkdir -p ${BCK_KERNEL_DIR}
-mv ${LIVE_DIR}/*.squashfs ${BCK_KERNEL_DIR}
-mv ${LIVE_DIR}/initrd* ${BCK_KERNEL_DIR}
-mv ${LIVE_DIR}/vmlinuz* ${BCK_KERNEL_DIR}
-
 ### Extract the kernel.
 unzip ${KERNELS_DIR}/022-kernel-686-pae.zip -d ${LIVE_DIR}
 
