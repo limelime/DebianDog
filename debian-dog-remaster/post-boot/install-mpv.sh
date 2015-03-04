b3f7bba40b68a6f275d9992fc52ee845d5bb2dc6
limelime <limelime@users.noreply.github.com>
2015-03-04 10:46:07 -0500

    Make mpv install simplier.

--- debian-dog-remaster/post-boot/install-mpv.sh
+++ debian-dog-remaster/post-boot/install-mpv.sh
@@ -3,6 +3,6 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 PACKAGE_DIR=${INSTALL_DATA_DIR}/packages
 
 ### Install mpv
-dpkg -i ${PACKAGE_DIR}/mpv_0.8.0dd_i386.deb
-# Install dependencies
-apt-get -y -f install
+# Need deb-multimedia repository.
+# mpv is from DebianDog repository.
+apt-get -y install mpv
