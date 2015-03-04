c6af43aca9950532b57b7a20a51ddcc76af82fa2
limelime <limelime@users.noreply.github.com>
2015-03-04 09:58:32 -0500

    Add comment and add tree

--- debian-dog-remaster/post-boot/installdev-debdog-dev-tools.sh
+++ debian-dog-remaster/post-boot/installdev-debdog-dev-tools.sh
@@ -4,4 +4,8 @@ SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 # Need debconf-get-selections
 apt-get -y install debconf-utils
 
+# GUI to set mount of partition
 apt-get -y install disk-manager
+
+# Display directory structure in the form of a tree
+apt-get -y install tree
