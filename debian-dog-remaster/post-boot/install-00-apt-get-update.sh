0dd72e35a34772d221c49d4fd0ec1c758c0039f8
limelime <limelime@users.noreply.github.com>
2015-03-05 12:55:31 -0500

    Upgrade Debian.

--- debian-dog-remaster/post-boot/install-00-apt-get-update.sh
+++ debian-dog-remaster/post-boot/install-00-apt-get-update.sh
@@ -6,11 +6,11 @@ DEB_CACHE_DIR=/var/cache/apt/archives/
 cp -Rf ${INSTALL_DATA_DIR}/apt-i386/archives/* ${DEB_CACHE_DIR} 
 
 ### Comment out deb-multimedia
-#SOURCE_LIST=/etc/apt/sources.list
-#cp ${SOURCE_LIST} ${SOURCE_LIST}.bck
-#sed -i "s/\(^deb.*deb-multimedia.*\)/#\1/" ${SOURCE_LIST}
+SOURCE_LIST=/etc/apt/sources.list
+cp ${SOURCE_LIST} ${SOURCE_LIST}.bck
+sed -i "s/\(^deb.*deb-multimedia.*\)/#\1/" ${SOURCE_LIST}
 
-### Update apt-get
+### Upgrade Debian
 apt-get update
 
 apt-get dist-upgrade
