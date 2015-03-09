ccac98bd5a8c847dfbd574747dc19c7560d1e3bc
limelime <limelime@users.noreply.github.com>
2015-03-09 14:51:33 -0400

    Auto install dist-upgrade.

--- debian-dog-remaster/post-boot/install-00-apt-get-update.sh
+++ debian-dog-remaster/post-boot/install-00-apt-get-update.sh
@@ -6,14 +6,16 @@ DEB_CACHE_DIR=/var/cache/apt/archives/
 cp -Rf ${INSTALL_DATA_DIR}/apt-i386/archives/* ${DEB_CACHE_DIR} 
 
 ### Comment out deb-multimedia
-#SOURCE_LIST=/etc/apt/sources.list
-#cp ${SOURCE_LIST} ${SOURCE_LIST}.bck
-#sed -i "s/\(^deb.*deb-multimedia.*\)/#\1/" ${SOURCE_LIST}
+SOURCE_LIST=/etc/apt/sources.list
+cp ${SOURCE_LIST} ${SOURCE_LIST}.bck
+sed -i "s/\(^deb.*deb-multimedia.*\)/#\1/" ${SOURCE_LIST}
+
+#dpkg -i ${INSTALL_DATA_DIR}/packages/deb-multimedia-keyring_2014.2_all.deb
 
 ### Upgrade Debian
 apt-get update
 
-apt-get dist-upgrade
+apt-get -y dist-upgrade
 
 ### Test
 # ls /var/cache/apt/archives/ | grep "\.deb"
