6e7f3977970d593d4967d01bc015ad3ddeedad33
limelime <limelime@users.noreply.github.com>
2015-03-09 10:52:16 -0400

    No need to correct sources.list.

--- debian-dog-remaster/post-boot/install-00-apt-get-update.sh
+++ debian-dog-remaster/post-boot/install-00-apt-get-update.sh
@@ -6,9 +6,9 @@ DEB_CACHE_DIR=/var/cache/apt/archives/
 cp -Rf ${INSTALL_DATA_DIR}/apt-i386/archives/* ${DEB_CACHE_DIR} 
 
 ### Comment out deb-multimedia
-SOURCE_LIST=/etc/apt/sources.list
-cp ${SOURCE_LIST} ${SOURCE_LIST}.bck
-sed -i "s/\(^deb.*deb-multimedia.*\)/#\1/" ${SOURCE_LIST}
+#SOURCE_LIST=/etc/apt/sources.list
+#cp ${SOURCE_LIST} ${SOURCE_LIST}.bck
+#sed -i "s/\(^deb.*deb-multimedia.*\)/#\1/" ${SOURCE_LIST}
 
 ### Upgrade Debian
 apt-get update
