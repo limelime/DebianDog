ae773a8677176314a77c88186190972528fcbe02
limelime <limelime@users.noreply.github.com>
2015-03-09 11:01:12 -0400

    Add kernel install script.

--- debian-dog-remaster/post-boot/install-00-00.sh
+++ debian-dog-remaster/post-boot/install-00-00.sh
@@ -2,6 +2,7 @@ INSTALL_DATA_DIR=./install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 # Execute base scripts in the following order:
+sh install-00-kernel.sh
 sh install-00-bashrc.sh
 sh install-00-proxy.sh
 sh install-00-apt-get-update.sh
