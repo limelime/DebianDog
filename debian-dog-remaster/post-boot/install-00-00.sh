2375aa8f0cd8148423782d7c8b9dee7dfc2eda4d
limelime <limelime@users.noreply.github.com>
2015-03-10 15:30:30 -0400

    Add comment.

--- debian-dog-remaster/post-boot/install-00-00.sh
+++ debian-dog-remaster/post-boot/install-00-00.sh
@@ -2,7 +2,8 @@ INSTALL_DATA_DIR=./install-data
 SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 # Execute base scripts in the following order:
-sh install-00-kernel.sh
+# 'sh install-00-kernel' line with change automatically by debian-dog-remaster.sh.
+sh install-00-kernel-686.sh
 sh install-00-bashrc.sh
 sh install-00-proxy.sh
 sh install-00-apt-get-update.sh
